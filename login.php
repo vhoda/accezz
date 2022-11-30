19dcea31a60ac759adc449bb9184d9cbb19949ce
vhoda <70240006+vhoda@users.noreply.github.com>
2022-11-30 02:51:31 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -20,6 +20,19 @@ if(isset($_SESSION["teacher_id"]))
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+  <script>
+        function info(){
+            swal({
+          title: "Accezz",
+          text: "Es una plataforma web que Registra y administra alumnos y docentes en 1 solo entorno",
+          icon: "info",
+          button: "OK",
+        });
+    }
+    </script>	
 </head>
 <body>
 <!--Login-->
@@ -53,6 +66,7 @@ if(isset($_SESSION["teacher_id"]))
             </div>
             <div class="mb-3 col-auto">
               <input type="submit" name="teacher_login" id="teacher_login" class="btn btn-dark" value="Ingresar" />
+              <a href="#" onclick="info()" ><i class="bi bi-info-square-fill text-dark" style="font-size: 1.5rem;"></i></a>
             </div>
           </form>
 
